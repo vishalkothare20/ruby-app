12b7de97491b110ab6f74163f664cd891b8d450c
Vishal Kothare <vishal.kothare20@gmail.com>
2017-10-04 20:43:52 +0530

    Changes in contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,7 +13,7 @@ class ContactsController < ApplicationController
   end
 
   private
-  def contact_params
-     params.require(:contact).permit(:name, :email, :comments)
-  end
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end

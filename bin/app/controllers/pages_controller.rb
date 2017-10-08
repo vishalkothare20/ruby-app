4234d1d429b7cc4cc27e24c5d9aac9f5c1095056
Vishal Kothare <vishal.kothare20@gmail.com>
2017-10-08 16:33:18 +0530

    Deleting extra files

--- bin/app/controllers/pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class PagesController < ApplicationController
-    def home
-        @basic_plan = Plan.find(1)
-        @pro_plan = Plan.find(2)
-    end
-
-    def about
-    end
-end
--- bin/app/models/contact.rb
+++
@@ -1,5 +0,0 @@
-class Contact < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :comments, presence: true
-end
